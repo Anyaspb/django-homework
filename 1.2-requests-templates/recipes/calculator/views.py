9e75c7865324b65176a2d111aeb127f67b6d0783
Anyaspb <124302415+Anyaspb@users.noreply.github.com>
2023-08-08 19:29:58 +0200

    Add files via upload

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -22,9 +22,19 @@ DATA = {
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+    # context = {
+    # 'recipe': {
+    #     'ингредиент1': количество1,
+    #     'ингредиент2': количество2,
+    #  }
+    # }
+
+def food(request, recipe_name):
+    qty = int(request.GET.get("servings", 1))
+    context = {
+        "recipe": DATA[recipe_name]
+    }
+
+    for x in context["recipe"]:
+        context["recipe"][x] = context["recipe"][x] * qty
+    return render(request, 'calculator/index.html', context)
