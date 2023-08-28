934ded145e783f3becf48c603302cf3345671194
Anyaspb <124302415+Anyaspb@users.noreply.github.com>
2023-08-29 01:39:12 +0200

    Add files via upload

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,28 @@
 from rest_framework import serializers
+from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ["id", "title", "description"]
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ["product", "quantity", "price"]
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ["id", "address", "positions"]
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,6 +34,13 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for item in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=item["product"],
+                quantity=item["quantity"],
+                price=item["price"]
+            )
 
         return stock
 
@@ -35,9 +50,13 @@ class StockSerializer(serializers.ModelSerializer):
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
         # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        stock_product_objects = StockProduct.objects.filter(stock=instance)
+        for item in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=item.get("product"), defaults={
+                "price": item.get("price"),
+                "quantity": item.get("quantity")
+            })
+
 
         return stock
